fbbf472e35441b75524b195280bd55bac541ca86
Vanja <vanjabrkans@gmail.com>
2025-11-17 07:57:03 +0100

    Add Wave Shape feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Navbar from "./components/Navbar/Navbar";
 import HeroArea from "./components/HeroArea/HeroArea";
 import FeatureArea from "./components/FeatureArea/FeatureArea";
 import Footer from "./components/Footer/Footer";
+import WaveShape from "./components/WaveShape/WaveShape";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <HeroArea />
+      <WaveShape color="#dfb0b0" flip bottomColor="#dfb0b0" className="wave-colorb"/>
       <FeatureArea />
+      <WaveShape color="#dfb0b0" className="wave-color" />
       <Footer />
     </div>
   );
