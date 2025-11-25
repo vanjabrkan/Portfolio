123aecf9fd2d1389ac7f9923c49992b5ccd63901
Vanja <vanjabrkans@gmail.com>
2025-11-25 17:44:11 +0100

    Refactor app structure by adding Layout (Navbar + Footer)

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home.jsx";
 import CV from "./Pages/CV.jsx";
 import Contact from "./Pages/Contact.jsx";
-import Login from "./pages/Login.jsx";
+import Login from "./Pages/Login.jsx";
+import Layout from "./Pages/Layout.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cv" element={<CV />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/cv" element={<CV />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
       </Routes>
     </Router>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
-import {Link} from "react-router-dom";
-import Navbar from "../components/Navbar/Navbar";
 import HeroArea from "../components/HeroArea/HeroArea";
 import FeatureArea from "../components/FeatureArea/FeatureArea";
-import Footer from "../components/Footer/Footer";
 import WaveShape from "../components/WaveShape/WaveShape";
 
 const Home = () => {
   return (
     <div className="Home">
-      <Navbar />
       <HeroArea />
       <WaveShape color="#dfb0b0" flip bottomColor="#dfb0b0" />
       <FeatureArea />
       <WaveShape color="#dfb0b0"  bottomColor="#dfb0b0" className="wave-color wave-up" />
-      <Footer />
     </div>
   )
 }
