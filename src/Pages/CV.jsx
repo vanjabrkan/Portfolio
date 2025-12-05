eb612a7ab1680940a04c2ae9e12939dc8ded4059
Vanja <vanjabrkans@gmail.com>
2025-12-05 13:02:14 +0100

    Add initial styling for CV page

--- src/Pages/CV.jsx
+++ src/Pages/CV.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./cv.css";
-
+import VanjaCV from "../assets/VanjaCV.jpg";
 const CV = () => {
   return (
     <div className="cv-container">
       <HeaderInfo />
+      <DownloadButton />
       <Skills />
       <Experience />
       <Education />
       <Languages />
       <Certifications />
       <Personal />
-      <DownloadButton />
     </div>
   );
 };
@@ -24,6 +24,7 @@ function HeaderInfo() {
       <div className="cv-title">
         <h1>Vanja Brkan Stankic</h1>
         <h2>Frontend Developer</h2>
+        <img src={VanjaCV} alt="Profile" className="cv-photo" />
 
         <p>
           Passionate developer with experience in React, UI/UX, and modern
@@ -35,7 +36,23 @@ function HeaderInfo() {
 }
 
 function Skills() {
-  const skills = ["React", "JavaScript", "HTML5", "CSS", "Git", "REST API"];
+  const skills = [
+    "React",
+    "JavaScript",
+    "HTML5",
+    "CSS",
+    "Git",
+    "REST API",
+    "DBeaver",
+    "pgAdmin",
+    "Cypress",
+    "Postman",
+    "Selenium WebDriver",
+    "JIRA",
+    "TestRail",
+    "DataDog",
+    "Murex",
+  ];
 
   return (
     <section className="cv-section">
@@ -60,32 +77,36 @@ function Experience() {
       <div className="cv-item">
         <h4>Software Test Developer — LEVI 9 GLOBAL SOURCING BALKAN DOO</h4>
         <span>2021 — 2024</span>
-        <p>
-          Responsibilities: <br />
-          - Utilized Swagger for validation of API endpoints <br />
-          - Performed API testing in Postman
-          <br />
-          - Developed End-to-End tests utilizing Cypress and Selenium WebDriver
-          <br />
-          - Created REST API tests with Cucumber
-          <br />
-          - Designed test cases and reported bugs effectively
-          <br />- Collaborated in Scrum methodology for agile development
-        </p>
+        <p>Responsibilities: </p>
+        <div className="cv-list cv-item">
+          <ul>
+            <li>Utilized Swagger for validation of API endpoints</li>
+            <li>Performed API testing in Postman</li>
+            <li>
+              Developed End-to-End tests utilizing Cypress and Selenium
+              WebDriver
+            </li>
+            <li>Created REST API tests with Cucumber</li>
+            <li>Designed test cases and reported bugs effectively</li>
+            <li>Collaborated in Scrum methodology for agile development</li>
+          </ul>
+        </div>
       </div>
 
       <div className="cv-item">
         <h4>IT Consultant — Synechron </h4>
         <span>2019 — 2021</span>
-        <p>
-          Responsibilites: <br />
-          - Provided IT solutions for Danish banks <br />
-          - Fixed production bugs
-          <br />
-          - Wrote optimized SQL queries, created views, and performed updates in
-          the database
-          <br />
-        </p>
+        <p>Responsibilites: </p>
+        <div className="cv-list cv-item">
+          <ul>
+            <li>Provided IT solutions for Danish banks</li>
+            <li>Fixed production bugs</li>
+            <li>
+              Wrote optimized SQL queries, created views, and performed updates
+              in the database
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   );
@@ -110,13 +131,12 @@ function Languages() {
     <section className="cv-section">
       <h3>Languages</h3>
 
-      <div className="cv-item">
-        <p>
-          Serbo-Croatian: Native language
-          <br />
-          English: Fluent <br />
-          German: B2
-        </p>
+      <div className="cv-list cv-item">
+        <ul>
+          <li>Serbo-Croatian: Native language</li>
+          <li>English: Fluent</li>
+          <li>German: B2</li>
+        </ul>
       </div>
     </section>
   );
@@ -127,22 +147,23 @@ function Certifications() {
     <section className="cv-section">
       <h3>Certifications</h3>
 
-      <div className="cv-item">
-        <p>
-          GOETHE-ZERTIFIKAT A2
-          <br />
-          Playwright JS Automation Testing from Scratch with Framework (Issuing
-          organization: Udemy)
-          <br />
-          GOETHE-ZERTIFIKAT A1 - START DEUTSCH 1<br />
-          Cypress: Web Automation Testing from Zero to Hero (Issuing
-          organization: Udemy)
-          <br />
-          Quick Introduction to Postman and API Testing for Beginners (Issuing
-          organization: Udemy)
-          <br />
-          Driver's license: Category B
-        </p>
+      <div className="cv-list cv-item">
+        <ul>
+          <li>
+            Playwright JS Automation Testing from Scratch with Framework
+            (Issuing organization: Udemy)
+          </li>
+          <li>GOETHE-ZERTIFIKAT A1 - START DEUTSCH 1</li>
+          <li>
+            Cypress: Web Automation Testing from Zero to Hero (Issuing
+            organization: Udemy)
+          </li>
+          <li>
+            Quick Introduction to Postman and API Testing for Beginners (Issuing
+            organization: Udemy)
+          </li>
+          <li>Driver's license: Category B</li>
+        </ul>
       </div>
     </section>
   );
@@ -152,17 +173,13 @@ function Personal() {
     <section className="cv-section">
       <h3>Personal</h3>
 
-      <div className="cv-item">
-        <p>
-          Board games
-          <br />
-          Photography
-          <br />
-          Reading books
-          <br />
-          Gardening and flower planting
-          <br />
-        </p>
+      <div className="cv-list cv-item">
+        <ul>
+          <li>Board games</li>
+          <li>Photography</li>
+          <li>Reading books</li>
+          <li>Gardening and flower planting</li>
+        </ul>
       </div>
     </section>
   );
@@ -171,7 +188,11 @@ function Personal() {
 function DownloadButton() {
   return (
     <div className="cv-download">
-      <a href="/Portfolio/Vanja Brkan Stankic CV.pdf" download className="download-btn">
+      <a
+        href="/Portfolio/Vanja Brkan Stankic CV.pdf"
+        download
+        className="download-btn"
+      >
         Download PDF
       </a>
     </div>
